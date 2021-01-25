84d993569154667c7b99e1eea05325d06ae11d70
Lucas <laksteelhouse@gmail.com>
2021-01-25 14:09:43 +0100

    Adding navigation, modifying the mainContainer on click, and adding active

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 const root = document.getElementById('root')
+const mainContainer = document.createElement('div')
+
+const addActive = (selectedTabLink) => {
+    let areActive = document.querySelectorAll('.active')
+    if (areActive.length > 0) {
+        areActive[0].classList.remove('active')
+    }
+    selectedTabLink.classList.add("active")
+}
 
 const header = () => {
     let headerContainer = document.createElement('div');
@@ -12,6 +21,10 @@ const header = () => {
         let tabLink = document.createElement("a");
         tabLink.classList.add("item");
         tabLink.innerText = tab
+        tabLink.addEventListener('click', () => {
+            startPage(tab);
+            addActive(tabLink)
+        })
         nav.appendChild(tabLink);
     });
 
@@ -20,15 +33,18 @@ const header = () => {
 };
 
 
-const startPage = () => {
-    let startPageContainer = document.createElement('div')
-    startPageContainer.classList.add('ui', 'container')
+const startPage = (selectedTab) => {
+    if (selectedTab === 'About Me') {
+        mainContainer.innerHTML = '<h2>About Me</h2> <p>Me humble self</p>'
+    } else if (selectedTab === 'My Projects') {
+        mainContainer.innerHTML = '<h2>My Projects</h2> <p>Prepare to be totally mindblown by my awesome skills</p>'
+    } else {
+        mainContainer.innerHTML = "<h1>Lucas's World</h1>"
+    };
+    
+    mainContainer.classList.add('ui', 'container')
 
-    let content = document.createElement('h1')
-    content.innerText = 'Hello World';
-
-    startPageContainer.appendChild(content)
-    root.appendChild(startPageContainer);
+    root.appendChild(mainContainer);
 };
 
 const footer = () => {
