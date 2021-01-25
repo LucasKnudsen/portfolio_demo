8477f5586e55afc731deae1b5a34f6899c740bce
Lucas Knudsen <laksteelhouse@gmail.com>
2021-01-25 11:40:17 +0100

    wtf..

--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
 const root = document.getElementById('root')
 
-const startPage () => {
+const header = () => {
+    let nav = document.createElement('nav')
+    let headerContent = document.createElement("h1");
+    headerContent.innerText = "My Portfolio";
+    nav.appendChild(headerContent);
+    root.appendChild(nav);
+};
+
+
+const startPage = () => {
     let content = document.createElement('h1')
     content.innerText = 'Hello World';
     root.appendChild(content);
 };
+
+const footer = () => {
+    let footer = document.create
+}
     
 document.addEventListener('DOMContentLoaded', () => {
+    header()
     startPage()
+    footer()
 });
