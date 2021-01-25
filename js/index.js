9b5ecec756e367c645158830e08be94e958893e0
Lucas Knudsen <laksteelhouse@gmail.com>
2021-01-25 11:26:18 +0100

    Writing first function

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 const root = document.getElementById('root')
+
+const startPage () => {
+    let content = document.createElement('h1')
+    content.innerText = 'Hello World';
+    root.appendChild(content);
+};
+    
+document.addEventListener('DOMContentLoaded', () => {
+    startPage()
+});
