7a0589e0bd3b3736d9f26ac524c9e66a3688ec2f
Lucas <laksteelhouse@gmail.com>
2021-01-25 15:44:50 +0100

    Starting homework of creating CV tab

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ const header = () => {
     let nav = document.createElement('div');
     nav.classList.add('ui', 'inverted', 'secondary', 'menu')
     
-    let tabs = ['My Portfolio', 'About Me', 'My Projects']
+    let tabs = ['My Portfolio', 'About Me', 'My Projects', 'My CV']
     tabs.forEach(tab => {
         let tabLink = document.createElement("a");
         tabLink.classList.add("item");
@@ -33,12 +33,15 @@ const header = () => {
 };
 
 
-const startPage = (selectedTab) => {
+const startPage = async (selectedTab) => {
     if (selectedTab === 'About Me') {
         mainContainer.innerHTML = '<h2>About Me</h2> <p>Me humble self</p>'
     } else if (selectedTab === 'My Projects') {
-        mainContainer.innerHTML = '<h2>My Projects</h2> <p>Prepare to be totally mindblown by my awesome skills</p>'
-    } else {
+        await displayProjects()
+    } else if (selectedTab === 'My CV') {
+        await displayCV()
+    } 
+    else {
         mainContainer.innerHTML = "<h1>Lucas's World</h1>"
     };
     
@@ -46,6 +49,42 @@ const startPage = (selectedTab) => {
 
     root.appendChild(mainContainer);
 };
+
+const displayProjects = async () => {
+    let projects = await fetchProjects()
+    mainContainer.innerHTML = ''
+    const projectsContainer = document.createElement('div')
+    projectsContainer.classList.add('ui', 'cards')
+
+    projects.forEach( project => {
+        let card = document.createElement('div')
+        let image = document.createElement('div')
+        let cardContent = document.createElement('div')
+        let cardDescription = document.createElement('div')
+        
+        card.classList.add('ui', 'card')
+        image.classList.add('image')
+        image.innerHTML = `<img src=${project.image}>`
+        cardContent.classList.add('content')
+        cardContent.innerHTML = `<div class="header">${project.title}</div>`
+        cardDescription.classList.add('description')
+        cardDescription.innerText = project.description
+
+        card.append(image, cardContent, cardDescription)
+        projectsContainer.appendChild(card)
+    })
+    
+    mainContainer.appendChild(projectsContainer)
+}
+
+const fetchProjects = async () => {
+    let response = await (await fetch('./js/projects.json')).json()
+    return response.projects
+}
+
+const displayCV = async () => {
+
+}
 
 const footer = () => {
     let footerContainer = document.createElement('footer')
